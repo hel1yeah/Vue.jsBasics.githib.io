3e972aa06426c22ba1965673dd1a1e6d63cb67a4
hel1yeah <hel1_yeah@ukr.net>
2020-10-11 01:55:58 +0300

    calc Vue js

---
+++ calcVue.js/main.js
@@ -0,0 +1,20 @@
+let app = new Vue({
+  el: '#app',
+  data: {
+    result: '',
+    numbers: [1,2,3,4,5,6,7,8,9,0],
+    operation: ['+', '-', '*', '/',]
+  },
+  methods: {
+    input:function(char) {
+      this.result = this.result.toString();
+      this.result += char;
+    },
+    reset: function(){
+      this.result = '';
+    },  
+    calc: function(){
+      this.result = eval(this.result);
+    },
+  }
+});

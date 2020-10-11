0be474c38a6b256db72e5709bcea8dc26564274d
hel1yeah <hel1_yeah@ukr.net>
2020-10-11 13:30:10 +0300

    little fix

--- calcVue.js/main.js
+++ calcVue.js/main.js
@@ -3,18 +3,24 @@ let app = new Vue({
   data: {
     result: '',
     numbers: [1,2,3,4,5,6,7,8,9,0],
-    operation: ['+', '-', '*', '/',]
+    operation: ['+', '-', '*', '/','(',')']
   },
   methods: {
     input:function(char) {
       this.result = this.result.toString();
       this.result += char;
     },
+    pi:function () {
+      this.result += 3.14;
+    },
     reset: function(){
       this.result = '';
     },  
     calc: function(){
-      this.result = eval(this.result);
+      this.result = Math.round(eval(this.result)* 1000)/1000;
     },
+    checkName: function(event){
+      
+    }
   }
 });
